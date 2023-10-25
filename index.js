0015ff7568d3d1c84e86b397db8872ab35152ce0
Olena Zdanovska <freshjs@gmail.com>
2023-10-25 10:16:22 +0300

    Add 404 & response for url

--- index.js
+++ index.js
@@ -2,15 +2,46 @@ const http = require('http');
 const fs = require('fs');
 const PORT = 3000;
 const requestListener = (request, response) => {
-  console.log('request ----->>>> ', request.url, request.method);
-  // console.log('response ----->>>> ', response);
-  fs.readFile('./views/index.html', {encoding: 'utf8'}, (err, data)=>{
-    if(err){
-      console.log(err);
+  const { url, method } = request;
+  if (method === 'GET') {
+    if (url === '/') {
+      fs.readFile('./views/index.html', { encoding: 'utf8' }, (err, data) => {
+        if (err) {
+          console.log(err);
+        }
+        response.end(data);
+      });
+      return;
     }
-    response.end(data);
-  })
+    if (url === '/about') {
+      fs.readFile('./views/about.html', { encoding: 'utf8' }, (err, data) => {
+        if (err) {
+          console.log(err);
+        }
+        response.end(data);
+      });
+      return;
+    }
+    if (url === '/contacts') {
+      fs.readFile('./views/contacts.html',{ encoding: 'utf8' },(err, data) => {
+          if (err) {
+            console.log(err);
+          }
+          response.end(data);
+        }
+      );
+      return;
+    }
+    fs.readFile('./views/404.html',{ encoding: 'utf8' },(err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      response.end(data);
+    }
+  );
+  }
 };
 const server = http.createServer(requestListener);
-server.listen(PORT, ()=>{console.log('server started!');});
-//console.log(server);
+server.listen(PORT, () => {
+  console.log('server started!');
+});
