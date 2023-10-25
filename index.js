58792026db29b8ad3da76545446450402edf9db8
Olena Zdanovska <freshjs@gmail.com>
2023-10-25 12:18:54 +0300

    Add post users

--- index.js
+++ index.js
@@ -1,45 +1,43 @@
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs').promises;
 const PORT = 3000;
-const requestListener = (request, response) => {
+const users = [];
+const routerGET = {
+  '/': './views/index.html',
+  '/about': './views/about.html',
+  '/contacts': './views/contacts.html',
+}
+const requestListener = async (request, response) => {
   const { url, method } = request;
   if (method === 'GET') {
-    if (url === '/') {
-      fs.readFile('./views/index.html', { encoding: 'utf8' }, (err, data) => {
-        if (err) {
-          console.log(err);
-        }
-        response.end(data);
+    try {
+      const data = await fs.readFile(routerGET[url], {
+        encoding: 'utf8',
       });
-      return;
+      return response.end(data);
+    } catch (error) {
+      console.log(error);
     }
-    if (url === '/about') {
-      fs.readFile('./views/about.html', { encoding: 'utf8' }, (err, data) => {
-        if (err) {
-          console.log(err);
-        }
-        response.end(data);
-      });
-      return;
-    }
-    if (url === '/contacts') {
-      fs.readFile('./views/contacts.html',{ encoding: 'utf8' },(err, data) => {
-          if (err) {
-            console.log(err);
-          }
-          response.end(data);
-        }
-      );
-      return;
-    }
-    fs.readFile('./views/404.html',{ encoding: 'utf8' },(err, data) => {
-      if (err) {
-        console.log(err);
-      }
-      response.end(data);
-    }
-  );
   }
+  if (method === 'POST') {
+    if (url === '/users') {
+      let jsonData = '';
+      request.on('data', (chunk) => {
+        jsonData += chunk;
+      });
+      request.on('end', () => {
+        const user = JSON.parse(jsonData);
+        console.log(user);
+        delete user.password;
+        user.id = Date.now();
+        users.push(user); //add to DB
+        console.log(users);
+        response.end(JSON.stringify(user));
+      });
+    }
+  }
+  const data = await fs.readFile('./views/404.html', { encoding: 'utf8' });
+  return response.end(data)
 };
 const server = http.createServer(requestListener);
 server.listen(PORT, () => {
